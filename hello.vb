fe034fc28303bbca4d966482049379d84e31fe1e
Cauê Alves <caue.mendes.rodrigues.alves@gmail.com>
2020-11-11 08:58:17 -0300

    Create hello.vb

---
+++ hello.vb
@@ -0,0 +1,7 @@
+Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        MessageBox.Show("Hello World", "Message Box Title")
+    End Sub
+
+End Class
